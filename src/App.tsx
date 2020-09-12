00c29d4d7d05440befbfbea85846520d513f6d80
James <jtlopez919@gmail.com>
2020-09-11 22:29:05 -0400

    fixed memory bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect,createRef} from 'react';
+import React,{useState} from 'react';
 import p5 from 'p5';
 import p5Types from 'p5';
 import Sketch from "react-p5";
@@ -9,6 +9,74 @@ import "p5/lib/addons/p5.sound";
 const rain = require('./rain.mp3');
 
 function App() {
+  const [soundFile,setSong] = useState<any>();
+  const [isSongLoaded,setLoaded] = useState<boolean>(true);
+
+
+  const preload = () =>{
+    setSong(new p5.SoundFile(rain,loaded))
+  }
+
+  let loaded = () =>{
+    console.log('song has loaded')
+    setLoaded(false);
+  }
+
+  const setup = (p5: p5Types,canvasParentRef: Element) => {
+      p5.createCanvas(100, 100).parent(canvasParentRef);
+      p5.background(220);
+      p5.text('tap here to play', 10, 20);
+ 
+  }; 
+
+  function play() {
+    soundFile.play();
+    soundFile.setVolume(0.1);
+  }
+
+  let stop = () =>{
+    soundFile.stop();
+  }
+
+
+ 
+
+  return (
+    <div className="App">
+      <header> <h1>Audio Visualizer</h1></header>
+      <main>
+        <div className="controller">
+          <div className="button-controller">
+          <button disabled={isSongLoaded} onClick={play}>Play audio</button>
+            <button disabled={isSongLoaded} onClick={stop}>Stop audio</button>
+      
+
+          </div>
+          <div className="volume-controller">
+            
+          </div>
+         
+
+        </div>
+
+        <div  className="visual-area">
+        {/* <P5Wrapper sketch={sketch}></P5Wrapper> */}
+     
+        <Sketch preload={preload} setup={setup} ></Sketch> 
+        </div>
+
+      </main>
+      <footer>
+
+        <h3>Copyright</h3>
+      </footer>
+    
+    </div>
+  );
+}
+
+export default App;
+
 
 //P5-WRAPPER VERSION
 
@@ -32,53 +100,3 @@ function App() {
   //     mySound.play();
   //   }
   // }
-
-
-  let p5sound = new p5.SoundFile(rain);
-
-
-  const setup = (p5: p5Types,canvasParentRef: Element) => {
-      let cnv = p5.createCanvas(100, 100).parent(canvasParentRef);
-      cnv.mousePressed(canvasPressed);
-      p5.background(220);
-      p5.text('tap here to play', 10, 20);
-  }; 
-
-  function canvasPressed() {
-    p5sound.play();
-  }
-
-  let stop = () =>{
-    p5sound.stop();
-  }
-
- 
-
-  return (
-    <div className="App">
-      <header> <h1>Audio Visualizer</h1></header>
-      <main>
-        <div className="controller">
-          <button>load audio</button>
-          <button onClick={stop}>Stop audio</button>
-          <button>Play audio</button>
-
-        </div>
-
-        <div  className="visual-area">
-        {/* <P5Wrapper sketch={sketch}></P5Wrapper> */}
-     
-        <Sketch setup={setup} ></Sketch>
-        </div>
-
-      </main>
-      <footer>
-
-        <h3>Copyright</h3>
-      </footer>
-    
-    </div>
-  );
-}
-
-export default App;
