a3e2935e80abc634426a89644a854646006792a0
James <jtlopez919@gmail.com>
2020-09-12 21:04:24 -0400

    volume slider and play stop state checking
    -functionality for volume
    -play stop buttons state check

--- src/App.tsx
+++ src/App.tsx
@@ -1,158 +1,116 @@
-import React,{useState} from 'react';
-import p5 from 'p5';
-import p5Types from 'p5';
+import React, { useState } from "react";
+import p5 from "p5";
+import p5Types from "p5";
 import Sketch from "react-p5";
-import './App.css';
+import "./App.css";
 // import P5Wrapper from 'react-p5-wrapper';
 import "p5/lib/addons/p5.sound";
 
-const rain = require('./audio/Mems2.wav');
+const rain = require("./audio/Mems2.wav");
 
 function App() {
-  const [soundFile,setSong] = useState<any>();
-  const [Amp,setAmp] = useState<any>(new p5.Amplitude());
-  const [FFT,setFFT] = useState<any>(new p5.FFT());
-  const [isSongLoaded,setLoaded] = useState<boolean>(true);
-  const [volHistory] = useState<any>([]);
+  const [soundFile, setSong] = useState<any>();
+  const [FFT, setFFT] = useState<any>(new p5.FFT());
+  const [isSongLoaded, setLoaded] = useState<boolean>(true);
+  const [volumeTemp, setVolumeTemp] = useState<number>(20);
+  const [isPlaying, setPlaying] = useState<boolean>(false);
 
+  let preload = () => {
+    setSong(new p5.SoundFile(rain, loaded));
+  };
 
-  const preload = () =>{
-    setSong(new p5.SoundFile(rain,loaded))
-  }
-
-  let loaded = () =>{
-    console.log('song has loaded')
+  let loaded = () => {
+    console.log("song has loaded");
     setLoaded(false);
-  }
+  };
 
-  const setup = (P5: p5Types,canvasParentRef: Element) => {
-      P5.createCanvas(700, 500).parent(canvasParentRef);
-      P5.angleMode(P5.DEGREES);
-      setAmp(new p5.Amplitude());
-      setFFT(new p5.FFT(0,512));
-  }; 
+  const setup = (P5: p5Types, canvasParentRef: Element) => {
+    P5.createCanvas(850, 500).parent(canvasParentRef);
+    P5.angleMode(P5.DEGREES);
+
+    P5.colorMode(P5.HSB);
+    setFFT(new p5.FFT(0, 256));
+    soundFile.setVolume(0.2);
+  };
 
   const draw = (p5: p5Types) => {
-    p5.background(50);
+    //SPECTRUM
+    let spectrum = FFT.analyze();
+    p5.translate(p5.width / 2, p5.height / 2);
+    p5.background(0);
 
-    //CENTER CIRCLE
-    let vol = Amp.getLevel();
-    let diam = p5.map(vol,0,0.5,10 ,200);
-    p5.fill(255,0,255);
-    p5.ellipse(p5.width/2,p5.height/2,diam*3,diam*3); 
-
-    //AMP LINE
-    volHistory.push(vol);   
-    p5.stroke(255);
-    p5.noFill();
-    p5.translate(p5.width/2,p5.height/2);
     p5.beginShape();
-    for(let i:number = 0; i < 360; i++){
-      let r = p5.map(volHistory[i],0,0.2,100,200);
-      let x = r * p5.cos(i);
-      let y = r * p5.sin(i);
-      // let y = p5.map(volHistory[i],0,0.2,p5.height/2,1);
-      p5.vertex(x,y);
-      
+    for (let i: number = 0; i < spectrum.length; i++) {
+      let angle = p5.map(i, 0, spectrum.length, 0, 360);
+      let amp = spectrum[i];
+      let r = p5.map(amp, 0, 200, 70, 240);
+      let x = r * p5.cos(angle);
+      let y = r * p5.sin(angle);
+      p5.stroke(i, 255, 255);
+      p5.line(0, 0, x, y);
     }
     p5.endShape();
-    
-    if(volHistory.length > 360){
-      volHistory.splice(0,1);
+  };
+
+  let play = () => {
+    if (!isPlaying) {
+      soundFile.play();
+      setPlaying(true);
     }
+  };
 
-
-        // let spectrum = FFT.analyze();
-    // let w = (p5.width / 60)-4;
-    // p5.stroke(255);
-    // for(let i:number = 0; i < spectrum.length; i++){
-    //   let amp = spectrum[i];
-    //   let y = p5.map(amp,0,256,p5.height,0);
-    //   p5.line(i*w,p5.height,i*w,y);
-
-    // }
-
-    
-};
-
-
-
-
-  function play() {
-    soundFile.play(); 
-    soundFile.setVolume(0.2);
-  }
-
-  let stop = () =>{
+  let stop = () => {
+    setPlaying(false);
     soundFile.stop();
-  }
+  };
 
-  let test = () =>{
-    console.log(volHistory);
-    volHistory.push(2);
-    console.log(volHistory);
+  let onChangeSlider = (e: any) => {
+    console.log(e.target.value / 100);
+    setVolumeTemp(e.target.value);
 
-  }
+    soundFile.setVolume(e.target.value / 100);
+  };
 
-
- 
- 
   return (
     <div className="App">
-      <header> <h1>Audio Visualizer</h1></header>
+      <header>
+        {" "}
+        <h1>Audio Visualizer</h1>
+      </header>
       <main>
         <div className="controller">
           <div className="button-controller">
-          <button disabled={isSongLoaded} onClick={play}>Play audio</button>
-            <button disabled={isSongLoaded} onClick={stop}>Stop audio</button>
-          <button disabled={isSongLoaded} onClick={test}>TEST Button</button>
-
+            <button disabled={isSongLoaded} onClick={play}>
+              Play audio
+            </button>
+            <button disabled={isSongLoaded} onClick={stop}>
+              Stop audio
+            </button>
           </div>
           <div className="volume-controller">
-            
+            <input
+              type="range"
+              min={0}
+              max={100}
+              value={volumeTemp}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                onChangeSlider(e)
+              }
+              className="slider"
+              id="myRange"
+            />
           </div>
-         
-
         </div>
 
-        <div  className="visual-area">
-        {/* <P5Wrapper sketch={sketch}></P5Wrapper> */}
-     
-        <Sketch preload={preload} setup={setup}  draw={draw }></Sketch> 
+        <div className="visual-area">
+          <Sketch preload={preload} setup={setup} draw={draw}></Sketch>
         </div>
-
       </main>
       <footer>
-
         <h3>Copyright</h3>
       </footer>
-    
     </div>
   );
 }
 
 export default App;
-
-
-//P5-WRAPPER VERSION
-
-  // const sketch = (p) =>{
-  //   let mySound;
-  //   p.preload = () =>{
-  //     p.soundFormats('wav');
-  //     mySound = p.loadSound(rain);
-      
-  //   }
-    
-  //   p.setup = () =>{
-  //     let cnv = p.createCanvas(100, 100);
-  //     cnv.mousePressed(canvasPressed);
-  //     p.background(220);
-  //     p.text('tap here to play', 10, 20);
-
-  //   }
-  //   function canvasPressed() {
-
-  //     mySound.play();
-  //   }
-  // }
